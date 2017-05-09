9992b2bcba0e7fde7236d4b36b0b2409cfa92193
bestsuperweb <bestsuperweb@gmail.com>
2017-05-09 10:32:28 +0000

    fixed the broken email address

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -5,12 +5,19 @@ ShopifyApp.configure do |config|
     config.secret = "d39c62acf9f6155036b4179fd11ee8f5"
     config.scope = "read_content, write_content, read_themes, write_themes, read_products, write_products, read_customers, write_customers, read_orders, write_orders, read_draft_orders, write_draft_orders, read_script_tags, write_script_tags, read_fulfillments, write_fulfillments, read_shipping, write_shipping, read_analytics"
     config.embedded_app = true
+    config.webhooks = [
+    {topic: 'orders/create', address: 'https://ruby-on-rail-sumonmg.c9users.io/webhooks/orders_create', format: 'json'},
+  ]
   elsif Rails.env.production?
     config.application_name = "CPI Test App on Heroku"
     config.api_key = "8d61f4cc21e49ec91dff4c139e780f08"
     config.secret = "29c03a9112790dbde6952a26a5b9a212"
     config.scope = "read_content, write_content, read_themes, write_themes, read_products, write_products, read_customers, write_customers, read_orders, write_orders, read_draft_orders, write_draft_orders, read_script_tags, write_script_tags, read_fulfillments, write_fulfillments, read_shipping, write_shipping, read_analytics"
     config.embedded_app = true
-  end
+    config.webhooks = [
+    {topic: 'orders/create', address: 'https://clippingpathindia.herokuapp.com/webhooks/orders_create', format: 'json'},
+  ]
+end
 
+  
 end
