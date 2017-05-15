508ca74c7b1a3b206c2dc4048274eb898f6b099a
bestsuperweb <bestsuperweb@gmail.com>
2017-05-15 06:21:55 +0000

    AppProxyAuth library

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,17 +4,19 @@ class DashboardController < ApplicationController
     
     include AppProxyAuth
     
+    require 'shopify_api'
+    
   def index
     
    @user_id = login_to_shopify('verify_logged_in_user')
    
   
    if(@user_id)
-      @customerorders = ShopifyAPI::Order.all(params: {id: @user_id, limit: 50, page: 1})
+        @customerorders = ShopifyAPI::Order.all(params: {id: @user_id, limit: 50, page: 1})
         render layout: true, content_type: 'application/liquid'
       else
-        
-          render :plain=> "Please don't try to override the url"
+        @error_msg ="Please don't try to override the url"
+        render '404/index.html', layout: true, content_type: 'application/liquid' 
          
       end
     
