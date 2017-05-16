1b0fc33fd996e42a8fec2940e81cb1e641108ef8
bestsuperweb <bestsuperweb@gmail.com>
2017-05-16 06:58:53 +0000

    Icon load issue

--- config/application.rb
+++ config/application.rb
@@ -8,21 +8,35 @@ Bundler.require(*Rails.groups)
 
 module Myapp
   class Application < Rails::Application
+    
     config.action_dispatch.default_headers['P3P'] = 'CP="Not used"'
     config.action_dispatch.default_headers.delete('X-Frame-Options')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    #config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     #config.assets.enabled = true  
     #config.assets.paths << "#{Rails.root}/app/assets/fonts"
     
     # Autoload lib/ folder including all subdirectories by shah alam
     config.autoload_paths += %W(#{config.root}/lib)
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
-    
+    config.serve_static_assets = true
     config.product_type ={ credit: "Credit bundles", product: "service", quotation: "QUOTE", credit_sku_prefix: "CREDIT_"  }
     
+     # Rails 5
+     config.middleware.insert 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+  
+
+    config.action_dispatch.default_headers = {
+        'Access-Control-Allow-Origin' => '*',
+        'Access-Control-Request-Method' => '*'
+    }
     
   end
 end
