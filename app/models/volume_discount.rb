e67a8a0c4e28be52b05cc3a5129c17565ad19525
bestsuperweb <bestsuperweb@gmail.com>
2017-08-21 15:57:08 +0000

    #27 & #30 completed

--- app/models/volume_discount.rb
+++ app/models/volume_discount.rb
@@ -1,3 +1,4 @@
 class VolumeDiscount < ApplicationRecord
     validates_uniqueness_of :name, :handle
+    validates :multiplier, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => {:greater_than => 0, :less_than => 10}
 end
