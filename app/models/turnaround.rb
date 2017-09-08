637943a5a6f584edc0c34e6867c2e0d65a352fb4
bestsuperweb <bestsuperweb@gmail.com>
2017-09-08 06:32:24 +0000

    turnaround available at price form validation fixed

--- app/models/turnaround.rb
+++ app/models/turnaround.rb
@@ -1,5 +1,5 @@
 class Turnaround < ApplicationRecord
     validates_uniqueness_of :name, :handle
     validates :multiplier, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => {:greater_than => 0, :less_than => 10}
-    validates :available_at_price, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => {:greater_than => 0, :less_than => 9999}
+    validates :available_at_price, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => {:greater_than_or_equal_to => 0, :less_than => 9999}
 end
