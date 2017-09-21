8d9283cdf8c290c4ebf6dc9d977c18346b92916a
bestsuperweb <bestsuperweb@gmail.com>
2017-09-21 19:41:18 +0000

    #24 completed

--- app/controllers/admin/quotations_controller.rb
+++ app/controllers/admin/quotations_controller.rb
@@ -2,8 +2,7 @@ class Admin::QuotationsController < ShopifyApp::AuthenticatedController
   layout 'admin'
 
   def index
-    sleep 2
-    @quotations = Quotation.all.order(updated_at: :desc).page(params[:page]).per(1)
+    @quotations = Quotation.all.order(updated_at: :desc).page(params[:page]).per(10)
   end
 
   def new
@@ -15,10 +14,10 @@ class Admin::QuotationsController < ShopifyApp::AuthenticatedController
     @images = Dir.glob("app/assets/images/"+@folder_path+"/*.jpg").sort
     render layout: false
   end
-  
+
   def search_filter
-    @quotations = Quotation.search(params[:filter], params[:search_box], params[:daterange_box]).order(updated_at: :desc).page(params[:page]).per(1)
-    render :template => 'admin/quotations/index' 
+    @quotations = Quotation.search(params[:filter], params[:search_box], params[:daterange_box]).order(updated_at: :desc).page(params[:page]).per(10)
+    render :template => 'admin/quotations/index'
   end
 
 end
